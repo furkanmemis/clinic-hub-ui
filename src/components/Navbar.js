0937aba5ad2855edf33b7950402e90597d1137c4
furkanmemis <fmemis0160@gmail.com>
2025-07-05 14:11:10 +0300

    Added clock

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,16 @@
+import { useEffect, useState } from "react";
 import AccountMenu from "./AccountMenu";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [time, setTime] = useState("");
+
+  useEffect(() => {
+    setTimeout(() => {
+      setTime(getTime());
+    }, 1000);
+  });
 
   const redirectToAppointment = () => {
     navigate("/appointment");
@@ -12,10 +20,30 @@ const Navbar = () => {
     navigate("/");
   };
 
+  const getTime = () => {
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, "0");
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const seconds = now.getSeconds().toString().padStart(2, "0");
+    return `${hours}:${minutes}:${seconds}`;
+  };
+
   return (
     <nav className="bg-green-500 h-15 shadow-md p-3 flex justify-between">
       <div className="text-white font-bold flex justify-start">
-        <p className="font-mono text-2xl" style={{cursor: "pointer"}} onClick={()=>{redirectToHome()}}>Clinic Hub</p>
+        <div className="flex flex-between">
+          <p
+            className="font-mono text-2xl"
+            style={{ cursor: "pointer" }}
+            onClick={() => {
+              redirectToHome();
+            }}
+          >
+            Clinic Hub
+          </p>
+          <p className="p-2">{time}</p>
+
+        </div>
       </div>
       <div className="flex justify-end">
         <button
