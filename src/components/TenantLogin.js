90529b55fd7ba38dd45914149c2786f809b2c92e
furkanmemis <fmemis0160@gmail.com>
2025-07-08 19:50:34 +0300

    Updated tenant login

--- src/components/TenantLogin.js
+++ src/components/TenantLogin.js
@@ -1,4 +1,6 @@
+import Typography from "@mui/material/Typography";
 import { useAuth } from "../Context/AuthContext";
+import Grid from "@mui/material/Grid";
 
 const TenantLogin = ({ tenantUser }) => {
   const { tenantLogin } = useAuth();
@@ -8,28 +10,43 @@ const TenantLogin = ({ tenantUser }) => {
   };
 
   return (
-    <div>
-      <div className="p-5">
-        <p className="text-center font-mono font-bold text-green-500">
+    <Grid container size={12}>
+      <Grid size={12} sx={{ display: "flex", justifyContent: "center" }}>
+        <Typography
+          style={{
+            fontFamily: "monospace",
+            fontWeight: "bold",
+            color: "darkblue",
+          }}
+        >
           Available Tenants For You
-        </p>
-      </div>
-      <div>
+        </Typography>
+      </Grid>
+
+      <Grid size={12} sx={{ display: "flex", justifyContent: "center", mt: 2 }}>
         {tenantUser.map((user, index) => {
           return (
-            <div
-              className="h-8 bg-green-400 border border-black text-center cursor-pointer"
+            <Grid
+              sx={{ backgroundColor: "darkblue", border: "3px solid black", width: "25vh", textAlign: "center", mb:2, cursor: "pointer" }}
               onClick={() => {
                 loginToTenant(user.email, user.tenantId);
               }}
               key={index}
             >
-              <p className="text-blue-800 font-mono font-bold uppercase p-1">{user.tenantId}</p>
-            </div>
+              <p
+                style={{
+                  fontFamily: "monospace",
+                  fontWeight: "bold",
+                  color: "white",
+                }}
+              >
+                {user.tenantId}
+              </p>
+            </Grid>
           );
         })}
-      </div>
-    </div>
+      </Grid>
+    </Grid>
   );
 };
 
